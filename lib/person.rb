bd1168d3e40c317f8531042b8b010649eb8012ab
Bernard065 <bebenibernard@gmail.com>
2023-02-09 07:52:52 +0300

    completed assignment

---
+++ lib/person.rb
@@ -0,0 +1,13 @@
+class Person
+    def talk
+        puts "Hello World!"
+    end
+
+    def walk
+        puts "The Person is walking"
+    end
+end
+
+bernard = Person.new
+bernard.talk
+bernard.walk
